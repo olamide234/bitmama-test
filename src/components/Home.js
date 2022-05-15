0ddbe868695f868375118a2455837320502550f6
Olamide Tofade <tofade.olamide2@gmail.com>
2022-05-15 23:53:27 +0100

    status display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,55 +1,67 @@
-import React, {useEffect, useState} from 'react'
-import {useNavigate, useLocation} from "react-router-dom";
-import "./Home.css"
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import "./Home.css";
 
-export default function Home({guest}) {
-    const[named, setNamed] = useState("")
-    let navigate = useNavigate()
-    let location = useLocation();
+export default function Home({ guest }) {
+  const [named, setNamed] = useState("");
+  const [active, setActive] = useState(true);
+  let navigate = useNavigate();
+  let location = useLocation();
 
-    useEffect(() => {
-        let abc = location.state
-        for (let i in abc){
-            setNamed(abc[i])
-        }
-    }, [location]);
-
-    const activeUsers = (arr) => {
-        const name = sessionStorage.getItem('name')
-        let index;
-        for (let i in arr) {
-            if (name === arr[i].username) {
-                index = i;
-            }
-        }
-        arr.splice(index, 1)
-        return arr;
+  useEffect(() => {
+    let abc = location.state;
+    for (let i in abc) {
+      setNamed(abc[i]);
     }
-    const onLogout = () => {
-        let previousUsers = JSON.parse(localStorage.getItem("allUsers"))
-        if (previousUsers.length > 1) {
-            let presentUsers = activeUsers(previousUsers)
-            localStorage.setItem("allUsers", JSON.stringify(presentUsers))
-        } else if (previousUsers.length === 1) {
-            localStorage.removeItem('allUsers');
-        }
+  }, [location]);
 
-        sessionStorage.removeItem('name');
-        navigate('/login');
+  const activeUsers = (arr) => {
+    const name = sessionStorage.getItem("name");
+    let index;
+    for (let i in arr) {
+      if (name === arr[i].username) {
+        index = i;
+      }
     }
-    
-    const onNewLogin = () => {
-        onLogout();
-        window.open(String(process.env.REACT_APP_REDIRECT_URI), "_self").focus()
+    arr.splice(index, 1);
+    return arr;
+  };
+  const onLogout = () => {
+    let previousUsers = JSON.parse(localStorage.getItem("allUsers"));
+    if (previousUsers.length > 1) {
+      let presentUsers = activeUsers(previousUsers);
+      localStorage.setItem("allUsers", JSON.stringify(presentUsers));
+    } else if (previousUsers.length === 1) {
+      localStorage.removeItem("allUsers");
     }
 
-    return (
-        <div className="bt__home">
-            <h1>Welcome, {guest || named}</h1>
-            <div className="bt__home-btns" >
-                <button onClick={onLogout}>Logout</button>
-                <button onClick={onNewLogin}> Login differently</button>
-            </div>
+    sessionStorage.removeItem("name");
+    navigate("/login");
+  };
+
+  const onNewLogin = () => {
+    onLogout();
+    window.open(String(process.env.REACT_APP_REDIRECT_URI), "_self").focus();
+  };
+  const mOut = () => {
+    setTimeout(() => setActive(false), 60000);
+  };
+  const mOver = () => {
+    setActive(true);
+  };
+
+  return (
+    <div className="bt__home" onMouseOut={mOut} onMouseOver={mOver}>
+      <div className="bt__home-content">
+        <div className="bt__home-content__text">
+          <h1>Welcome, {guest || named}</h1>
+          <button>{active ? "Active" : "Idle"}</button>
         </div>
-    )
+        <div className="bt__home-content__btns">
+          <button onClick={onLogout}>Logout</button>
+          <button onClick={onNewLogin}> Login differently</button>
+        </div>
+      </div>
+    </div>
+  );
 }
